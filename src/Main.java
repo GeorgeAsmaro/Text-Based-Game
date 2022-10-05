7f4f806742e42e7bef07a7f040cccdd386c1e3fc
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-05 09:48:21 -0700

    ..

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,13 @@ public class Main {
 
     static Scanner input = new Scanner(System.in);
     static Random rand = new Random();
-    public static void main(String[] args)
 
-    {
+    public static void main(String[] args) {
         System.out.println("Pick a number from 0-49");
         int oneToTen = 51;
-        int random = rand.nextInt(50);
-
-        while(oneToTen != random) {
-
-            System.out.println("Wrong, guess again");
-            input.nextInt();
-
-            if(oneToTen == random) {
-                System.out.println("Correct!");
-                break;
-            }
+        int random = rand.nextInt(10);
 
 
-        }
     }
+
 }
