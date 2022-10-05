81ea8a2a72a51fe244adeb72208b01dc0a688e42
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-05 09:41:43 -0700

    ...

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main {
 
-    Scanner input = new Scanner(System.in);
-    Random rand = new Random();
+    static Scanner input = new Scanner(System.in);
+    static Random rand = new Random();
     public static void main(String[] args)
 
     {
-        System.out.println("Pick a number from 1-10");
+        System.out.println("Pick a number from 0-49");
+        int oneToTen = 51;
+        int random = rand.nextInt(50);
+
+        while(oneToTen != random) {
+
+            System.out.println("Wrong, guess again");
+            input.nextInt();
+
+            if(oneToTen == random) {
+                System.out.println("Correct!");
+                break;
+            }
+
+
+        }
     }
 }
