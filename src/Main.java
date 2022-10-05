ff7653a3c7e748039b1e694d3dcc29cc69c3c3f9
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-05 09:28:01 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
+
+    Scanner input = new Scanner(System.in);
+    Random rand = new Random();
     public static void main(String[] args)
 
     {
-        System.out.println("Hello world!");
-        System.out.println("Testing");
+        System.out.println("Pick a number from 1-10");
     }
 }
