a44d97c553ddb3e861b7e796b8ae9b2a9eece971
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-12 09:09:15 -0700

    fixes

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,19 @@ public class Main {
     static Random rand = new Random();
 
     public static void main(String[] args) {
-        System.out.println("Pick a number from 0-49");
-        int oneToTen = 51;
-        int random = rand.nextInt(10);
+        System.out.println("Pick a number from 1-10");
+        int oneToTen = 0;
+        int random = rand.nextInt(10) + 1;
         oneToTen = input.nextInt();
         while(oneToTen != random) {
             oneToTen = input.nextInt();
 
             if (oneToTen == random) {
-                System.out.println("You have guessed the correct number! The number was: " + random);
                 break;
             }
+        }
+        if (oneToTen == random) {
+            System.out.println("You have guessed the correct number! The number was: " + random);
         }
 
 
