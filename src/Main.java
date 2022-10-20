f5896da5eaa4e0cebcc698f062abdad0da7d442c
George Asmaro <george.asmaro@hotmail.com>
2022-10-20 12:13:19 -0700

    Fixes and updates and new features

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,61 @@ public class Main {
     static Scanner input = new Scanner(System.in);
     static Random rand = new Random();
     static ExceptionHandling  exceptionHandling = new ExceptionHandling();
-    public static void main(String[] args) {
 
+    static boolean countryChosen = false;
+    public static void main(String[] args) {
+        mainMenu();
+    }
+
+    public static void newGame() {
+        System.out.println("New game Started!");
+        System.out.println("");
+
+        System.out.println("What is your character's name?");
+        input.nextLine();
+        String playerName = input.nextLine();
+
+        System.out.println("");
+        System.out.println("Your character's name is now set to: " + playerName);
+        System.out.println("");
+
+        chooseCountry();
+
+
+        
+
+    }
+
+    public static void continueGame() {
+        System.out.println("No current save, what would you like to do?");
+
+        System.out.println("1. Back to main menu");
+        System.out.println("2. New Game");
+        System.out.println("3. Exit");
+
+        int whatNext = input.nextInt();
+
+        if(whatNext == 1) {
+            mainMenu();
+        }
+        else if(whatNext == 2) {
+            newGame();
+        }
+        else if (whatNext == 3) {
+            exitGame();
+        }
+
+    }
+
+    public static void instructions() {
+        System.out.println("Instructions:");
+    }
+
+    public static void exitGame() {
+
+    }
+
+    public static void mainMenu() {
         System.out.println("");
         System.out.println("Start Menu");
         System.out.println("");
@@ -33,32 +86,32 @@ public class Main {
                     System.out.println("Incorrect input! Please input a number from 1-4.");
                 }
 
-                    switch(num) {
+                switch(num) {
 
-                        case 1: {
-                            correctInput = true;
-                            newGame();
-                            break;
-                        }
-
-                        case 2: {
-                            correctInput = true;
-                            continueGame();
-                            break;
-                        }
-
-                        case 3: {
-                            correctInput = true;
-                            instructions();
-                            break;
-                        }
-
-                        case 4: {
-                            correctInput = true;
-                            exitGame();
-                            break;
-                        }
+                    case 1: {
+                        correctInput = true;
+                        newGame();
+                        break;
                     }
+
+                    case 2: {
+                        correctInput = true;
+                        continueGame();
+                        break;
+                    }
+
+                    case 3: {
+                        correctInput = true;
+                        instructions();
+                        break;
+                    }
+
+                    case 4: {
+                        correctInput = true;
+                        exitGame();
+                        break;
+                    }
+                }
             }
 
             catch(InputMismatchException exception) {
@@ -74,14 +127,7 @@ public class Main {
 
     }
 
-    public static void newGame() {
-        System.out.println("New game Started!");
-        System.out.println("");
-
-        System.out.println("What is your character's name?");
-        input.nextLine();
-        String playerName = input.nextLine();
-
+    public static void chooseCountry() {
         System.out.println("What country is your character from?");
 
         System.out.println("1. Canada");
@@ -91,7 +137,6 @@ public class Main {
 
         String playerCountry = "";
 
-        boolean countryChosen = false;
 
         while(!countryChosen) {
             playerCountry  = input.nextLine();
@@ -101,57 +146,101 @@ public class Main {
                 case "1" -> {
                     playerCountry = "Canada";
                     Player player = new Player(0,playerCountry);
-                    countryChosen = true;
+
+                    System.out.println("Country is set to " + playerCountry + ". Are you sure?");
+
+                    System.out.println("1. Yes");
+
+                    System.out.println("2. No, change it");
+
+                    int changeCountry = input.nextInt();
+
+                    if(changeCountry == 2) {
+                        countryChosen = false;
+                        chooseCountry();
+                    }
+                    else {
+                        countryChosen = true;
+
+                        System.out.println("Your character's country is now set to: " + playerCountry);
+                    }
+
+
                 }
 
                 case "2" -> {
                     playerCountry = "Australia";
                     Player player = new Player(0,playerCountry);
-                    countryChosen = true;
+
+                    System.out.println("Country is set to " + playerCountry + ". Are you sure?");
+
+                    System.out.println("1. Yes");
+
+                    System.out.println("2. No, change it");
+
+                    int changeCountry = input.nextInt();
+
+                    if(changeCountry == 2) {
+                        countryChosen = false;
+                        chooseCountry();
+                    }
+                    else {
+                        countryChosen = true;
+
+                        System.out.println("Your character's country is now set to: " + playerCountry);
+                    }
                 }
 
                 case "3" -> {
                     playerCountry = "United States of America";
                     Player player = new Player(0,playerCountry);
-                    countryChosen = true;
+
+                    System.out.println("Country is set to " + playerCountry + ". Are you sure?");
+
+                    System.out.println("1. Yes");
+
+                    System.out.println("2. No, change it");
+
+                    int changeCountry = input.nextInt();
+
+                    if(changeCountry == 2) {
+                        countryChosen = false;
+                        chooseCountry();
+                    }
+                    else {
+                        countryChosen = true;
+
+                        System.out.println("Your character's country is now set to: " + playerCountry);
+                    }
                 }
 
                 case "4" -> {
                     System.out.println("What country is your character from?");
                     playerCountry = input.nextLine();
                     Player player = new Player(0,playerCountry);
-                    countryChosen = true;
+
+                    System.out.println("Country is set to " + playerCountry + ". Are you sure?");
+
+                    System.out.println("1. Yes");
+
+                    System.out.println("2. No, change it");
+
+                    int changeCountry = input.nextInt();
+
+                    if(changeCountry == 2) {
+                        countryChosen = false;
+                        chooseCountry();
+                    }
+                    else {
+                        countryChosen = true;
+
+                        System.out.println("Your character's country is now set to: " + playerCountry);
+                    }
                 }
 
             }
-
-            System.out.println("Country is set to: " + playerCountry + ". Are you sure?");
-
-            System.out.println("1. Yes");
-
-            System.out.println("2. No, change it");
-
-            int changeCountry = input.nextInt();
-
-            if(changeCountry == 2) {
-                countryChosen = false;
-            }
         }
 
-
-        
-
-    }
-
-    public static void continueGame() {
-
-    }
-
-    public static void instructions() {
-        System.out.println("Instructions:");
-    }
-
-    public static void exitGame() {
 
     }
 
